f59addf21df33ec8fb858367e1f7af28f9204d59
Micah Elm <micah.b.elm@gmail.com>
2023-08-17 15:10:42 -0700

    transfer mobile nav details to MobileNav component

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,12 @@
+
+const MobileNav = () =>{
+    return(
+        <nav className="mobile-nav" role='navigation'>
+        <ul className='mobile-nav--list'>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+}
+export default MobileNav
