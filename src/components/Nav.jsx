5b08673f3b25851bbe2bd9429531156a6216f8c4
Micah Elm <micah.b.elm@gmail.com>
2023-08-17 15:12:47 -0700

    remove parent wrapper | parent wrapper stays in parent component of Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,12 @@
 
 const Nav = ()=>{
     return(
-        <div className='site-nav'>
         <nav className='nav' role='navigation'>
           <ul>
             <li>About</li>
             <li>Contact</li>
           </ul>
         </nav>
-      </div>
     )
 }
 
