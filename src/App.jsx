7b1bc3b24e25affbfddb886e5a98c762baf33931
Micah Elm <micah.b.elm@gmail.com>
2023-08-17 15:25:41 -0700

    remove welcome content details | import WelcomeComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import WelcomeContent from './components/WelcomeContent'
 import './App.css'
 
 function App() {
@@ -11,25 +12,7 @@ function App() {
       <Header />
      </div> 
      <div id="site-content">
-      <section className='welcome--section'>
-        <div className="welcome--left-title">
-          <h1>Who we are</h1>
-          <h1>Our mission</h1>
-        </div>
-        <div className="welcome--right-info">
-          <div className="welcome--right-who">
-            <div className="welcome--right-who-wrapper">
-              <p className='welcome--right-who-info'>Woman-Owned</p>
-            </div>
-          </div>
-          <div className='welcome--right-blocker'></div>
-          <div className="welcome--right-mission">
-            <div className="welcome--right-mission-wrapper">
-              <p className="welcome--right-mission-info">Cultured inspired treats</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <WelcomeContent />
      </div>
      <Footer />
     </>
