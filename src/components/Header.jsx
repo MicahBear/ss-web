48e4d8e45d61830f25b0102fde75b12c0ab96ec6
Micah Elm <micah.b.elm@gmail.com>
2023-08-17 15:08:32 -0700

    transfer header details to Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = ()=>{
+    return(
+        <header>
+        <div className='site-logo'>
+          <div itemProp='name'>
+            <a itemprop='url' href="https://steffisweets.com" role='link'>
+              <img itemprop='logo' src='' alt='Steffi Sweets' />
+            </a>
+          </div>
+        </div>
+        <div className='site-nav'>
+          <nav className='nav' role='navigation'>
+            <ul>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+          <nav className="mobile-nav" role='navigation'>
+            <ul className='mobile-nav--list'>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+       </header>
+    )
+}
+
+export default Header
