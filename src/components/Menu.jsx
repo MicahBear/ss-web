5ee2901ce6372214d4bc4ac4dc3ef7b657f2a2fb
Micah Elm <micah.b.elm@gmail.com>
2023-08-23 15:36:38 -0700

    import Menu component to App.jsx | import Menu.css to Menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import './Menu.css'
+const Menu = () =>{
+    return(
+        <section className="menu--container">
+            <h3 className="">Menu</h3>
+            <ul className="menu--list">
+                <li className="menu--list-item">ube cinny bun</li>
+                <li className="menu--list-item">calamansi muffins</li>
+                <li className="menu--list-item">beignets</li>
+                <li className="menu--list-item">apple roses</li>
+                <li className="menu--list-item">ube-macapun suman</li>
+                <li className="menu--list-item">puto bumbong</li>
+                <li className="menu--list-item">champorado suman</li>
+                <li className="menu--list-item">mango suman</li>
+                <li className="menu--list-item">espasol</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Menu
