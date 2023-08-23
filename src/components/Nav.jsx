aabec2d596460f981968108c2cbdd675251ef685
Micah Elm <micah.b.elm@gmail.com>
2023-08-23 09:47:43 -0700

    create WelcomeContent.css file for WelcomeContent component | update welcome content mission info | update welcome content who info

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,7 @@ const Nav = ()=>{
         <nav className='nav' role='navigation'>
           <ul className="nav--list">
             <li className="nav--list-item"><a href="">About</a></li>
+            <li className="nav--list-item"><a href="">Menu</a></li>
             <li className="nav--list-item"><a href="">Contact</a></li>
           </ul>
         </nav>
--- src/components/WelcomeContent.jsx
+++ src/components/WelcomeContent.jsx
@@ -9,13 +9,13 @@ const WelcomeContent = () =>{
         <div className="welcome--right-info">
           <div className="welcome--right-who">
             <div className="welcome--right-who-wrapper">
-              <p className='welcome--right-who-info'>Woman-Owned</p>
+              <p className='welcome--right-who-info'>Family run business based in Long Beach, Ca. We specialize in naturally leavened desserts or Kakanins (Filipino desserts made of rice and coconut milk.)</p>
             </div>
           </div>
           <div className='welcome--right-blocker'></div>
           <div className="welcome--right-mission">
             <div className="welcome--right-mission-wrapper">
-              <p className="welcome--right-mission-info">Cultured inspired treats</p>
+              <p className="welcome--right-mission-info">Community is everything, and with the guidance of my ancestors I try to incorporate and embrace the tranditional flavors and foods from my culture.</p>
             </div>
           </div>
         </div>
