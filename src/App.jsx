2b99c1d31da68213f5fe7b21de367a7b4f009ffb
Micah Elm <micah.b.elm@gmail.com>
2023-08-28 09:39:01 -0700

    adding images to app.jsx for  review

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import WelcomeContent from './components/WelcomeContent'
+import Menu from './components/Menu'
 import './App.css'
 
 function App() {
@@ -11,8 +12,29 @@ function App() {
      <div className="header--parent">
       <Header />
      </div> 
+     <div className="image--break">
+     <div className="image--parent">
+            <div className="image--link-parent">
+                <a href="http://">
+                    <img src="../src/assets/ubeBeignets.webp" alt="" />
+                </a>
+            </div>
+        </div>
+     </div>
      <div id="site-content">
       <WelcomeContent />
+     </div>
+     <div className="image--break">
+     <div className="image--parent">
+            <div className="image--link-parent">
+                <a href="http://">
+                    <img src="../src/assets/ubeBiko.webp" alt="" />
+                </a>
+            </div>
+        </div>
+     </div>
+     <div id="menu-content">
+      <Menu />
      </div>
      <div className="footer--parent">
       <Footer />
