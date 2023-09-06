c88c0036b12c92a55fd677668167d24a7b66c068
Micah Elm <micah.b.elm@gmail.com>
2023-09-06 16:19:10 -0700

    update placement of nav links - title - and mobile option - made nav-list display none when screen size met

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,15 @@ const Nav = ()=>{
     return(
         <nav className='nav' role='navigation'>
           <ul className="nav--list">
-            <li className="nav--list-item"><a href="">About</a></li>
-            <li className="nav--list-item"><a href="#menu">Menu</a></li>
-            <li className="nav--list-item"><a href="">Contact</a></li>
+            <li className="nav--list-item"><a href="">about</a></li>
+            <li className="nav--list-item"><a href="#menu">menu</a></li>
+            <li className="nav--list-item"><a href="">contact</a></li>
           </ul>
+   
+          <div className="nav--title-content">
+           <h1 className="nav--title">Steffi Sweets</h1>
+           </div>
+           <i className="fa-solid fa-bars pl-3 "></i>
         </nav>
     )
 }
